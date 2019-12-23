6d9154474ebb672c19f635c34e310792529eb8d5
Lyesin <ylyesin@corp.bcimc.com>
2019-12-23 15:55:16 -0800

    adtional tests for SEC_13F_list to check for NA

--- tests/testthat/test-Pass_0_0_into_function_call.R
+++ tests/testthat/test-Pass_0_0_into_function_call.R
@@ -4,4 +4,6 @@ test_that("0,0 and other into function call", {
   expect_error(SEC13Flist::SEC_13F_list(2005,5))
   expect_error(SEC13Flist::SEC_13F_list(2004,0))
   expect_error(SEC13Flist::SEC_13F_list(0,1))
+  expect_error(SEC13Flist::SEC_13F_list(2004,"a"))
+  expect_error(SEC13Flist::SEC_13F_list("b","2"))
 })
