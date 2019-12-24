92de24d1460703eb7865ce6866d2509555ae0ad4
Lyesin <ylyesin@corp.bcimc.com>
2019-12-23 16:30:00 -0800

    more tests for isCusip function

--- tests/testthat/test-valid_cusip.R
+++ tests/testthat/test-valid_cusip.R
@@ -1,4 +1,8 @@
 test_that("Valid CUSIP", {
   expect_equal(SEC13Flist::isCusip("B38564108"), TRUE)
   expect_equal(SEC13Flist::isCusip("B38564109"), FALSE)
+  expect_equal(SEC13Flist::isCusip("123456#13"), TRUE)
+  expect_equal(SEC13Flist::isCusip("123456#14"), FALSE)
+  expect_equal(SEC13Flist::isCusip("123456*15"), TRUE)
+  expect_equal(SEC13Flist::isCusip("123456*16"), FALSE)
 })
