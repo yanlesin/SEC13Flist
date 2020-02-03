5023b5650912c4c93319535385655c73a9d4f127
Lyesin <ylyesin@corp.bcimc.com>
2020-02-03 15:39:06 -0800

    cran comments 2

--- R/sec_13f_list.R
+++ R/sec_13f_list.R
@@ -1,6 +1,6 @@
 #' @title Official List of Section 13(f) Securities
 #'
-#' @description This function downloads, specified by Year and Quarter, Official List of Section 13(f) Securities from SEC website, parses it and returns a data frame. If no parameters provided, function determines year and quarter based on Current List section of SEC website
+#' @description This function downloads, specified by Year and Quarter (starting from year 2004, quarter 1), official list of Section 13(f) securities from SEC website <https://www.sec.gov/divisions/investment/13flists.htm>, parses it and returns a data frame. If no parameters provided, function determines year and quarter based on Current List section of SEC website <https://www.sec.gov/divisions/investment/13flists.htm>
 #' @param YEAR_ Numeric, Year for the SEC List
 #' @param QUARTER_ Numeric, Quarter for the SEC List
 #' @param show_progress Logical, Show progress during list parsing, default value show_progress = FALSE
@@ -9,11 +9,11 @@
 #' \itemize{
 #' \item \code{CUSIP}: character - CUSIP number of the security included in the official list
 #' \item \code{HAS_LISTED_OPTION}: character - An asterisk idicates that security having a listed option and each option is individually listed with its own CUSIP number immediately below the name of the security having the option
-#' \item \code{ISSUER_NAME}: character - Issuer Name
-#' \item \code{ISSUER_DESCRIPTION}: character - Issuer Description
+#' \item \code{ISSUER_NAME}: character - Issuer name
+#' \item \code{ISSUER_DESCRIPTION}: character - Issuer description
 #' \item \code{STATUS}: character - "ADDED" (The security has become a Section 13(f) security) or "DELETED" (The security ceases to be a 13(f) security since the date of the last list)
-#' \item \code{YEAR}: integer - Year of the list
-#' \item \code{QUARTER}: integer - Quarter of the list
+#' \item \code{YEAR}: integer - Year of the official list
+#' \item \code{QUARTER}: integer - Quarter of the official list
 #' }
 #' @export
 #' @examples
