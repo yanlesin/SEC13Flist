3399dda7f3b0db7029ccf6dfbfea32bd9583b96f
Yan Lyesin <yanlyesin@Yans-MacBook-Pro.local>
2021-03-17 20:23:13 -0700

    added adjustment to handle different download routine on Windows to reslove failure of build on windows

--- tests/testthat/test-sec13F-local.R
+++ tests/testthat/test-sec13F-local.R
@@ -1,6 +1,8 @@
 test_that("Local processing yilds same result as via web-site", {
-  download.file("https://www.sec.gov/divisions/investment/13f/13flist2020q3.pdf", destfile = "13flist2020q3.pdf")
-aa <- SEC13Flist::SEC_13F_list_local("13flist2020q3.pdf")
+  if (Sys.info()["sysname"] == "Windows") download.file("https://www.sec.gov/divisions/investment/13f/13flist2020q3.pdf",
+                                                        mode = "wb",
+                                                        destfile = "13flist2020q3.pdf") else download.file("https://www.sec.gov/divisions/investment/13f/13flist2020q3.pdf",
+                                                                                                           destfile = "13flist2020q3.pdf")aa <- SEC13Flist::SEC_13F_list_local("13flist2020q3.pdf")
 bb <- SEC13Flist::SEC_13F_list(2020, 03)
 expect_equal(aa,bb)
 
