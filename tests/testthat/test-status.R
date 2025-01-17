35b2b26e7beb8fbc9356f24249bc49995e189729
Yan Lyesin <yanlyesin@Yans-MacBook-Pro-2.local>
2025-01-16 21:00:40 -0800

    Q4 2024 file tested

--- tests/testthat/test-status.R
+++ tests/testthat/test-status.R
@@ -3,7 +3,7 @@ test_that("list of possible values for STATUS column", {
   STATUS_1 <- ""
   STATUS_2 <- "ADDED"
   STATUS_3 <- "DELETED"
-  list_all <- SEC13Flist::SEC_13F_list(2024, 3)
+  list_all <- SEC13Flist::SEC_13F_list(2024, 4)
   list_all <- unique(list_all["STATUS"])
   list_STATUS <- nrow(subset(list_all, !STATUS %in% c(STATUS_1,STATUS_2,STATUS_3)))
   expect_equal(list_STATUS, 0)
